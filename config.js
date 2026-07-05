7719df4615a47d79ec2e5e261ba782f045acf7d8
anthony <anthonyogbuah@gmail.com>
2026-07-05 18:45:10 +0300

    Add public Supabase config

---
+++ config.js
@@ -0,0 +1,5 @@
+window.env = {
+    SUPABASE_URL: "https://jneverdaiaykzywxlmbk.supabase.co",
+    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpuZXZlcmRhaWF5a3p5d3hsbWJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxNzcyODIsImV4cCI6MjA5ODc1MzI4Mn0.elfjZTKfs-k0qrgfpAMY9H4B12anzuosBOGB7pcBH9M"
+};
+
